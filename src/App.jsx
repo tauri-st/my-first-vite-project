ecb35556e41a85a6e5a8c411987d6b6679263589
tauri_s <tauristclaire@gmail.com>
2023-12-05 18:08:18 -0800

    Import react confetti library into the App.jsx and return the confettin component in the App functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Confetti from "react-confetti";
 
 function App() {
+  
   return (
     <>
       <h1>Wow, I'm working with React on my machine!</h1>
       <p>Is this what happiness feels like?</p>
+      <Confetti />
     </>
   )
 }
