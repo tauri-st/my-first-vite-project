01f4cc6a0973b790a9ac66c2caf0b01de5952180
tauri_s <tauristclaire@gmail.com>
2023-12-05 18:01:10 -0800

    Add return and jsx tags back in and add h1 and p text

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  return (
+    <>
+      <h1>Wow, I'm working with React on my machine!</h1>
+      <p>Is this what happiness feels like?</p>
+    </>
+  )
 }
 
 export default App
